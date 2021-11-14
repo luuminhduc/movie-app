7e152d37acebcad3a8da4e755d7b777dd70410fc
Duc Luu <luuminhduc00@gmail.com>
2021-11-14 20:40:45 +0700

    Fix bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,25 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducer/index';
-const middleware = [thunk];
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+// import rootReducer from './reducer/index';
+// const middleware = [thunk];
 
-const store = createStore(rootReducer,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    )
-)
+// const store = createStore(rootReducer,
+//     compose(
+//         applyMiddleware(...middleware),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//     )
+// )
+
+// export default store;
+
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducer/index";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
